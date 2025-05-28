b688d14bca158680fcef5460c03bb7ec6c8c04d5
WhoAmI1610 <whoami0161@gmail.com>
2025-05-28 15:18:26 -0500

    Add files via upload

---
+++ baseDatos(Emanuel)/src/main/java/com/usta/proyectofinal/entities/RolEntity.java
@@ -0,0 +1,44 @@
+package com.usta.proyectofinal.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "ROLES")
+
+public class RolEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol")
+    private Long idRol;
+
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "rol", length = 30, nullable = false)
+    private String rol;
+
+    public RolEntity(String rol) {
+        super();
+        this.rol = rol;
+    }
+    public RolEntity(){
+
+    }
+    public String getRol() {
+        return this.rol;
+    }
+    public void setRol(String rol){
+        this.rol = rol;
+    }
+
+    public Long getIdRol(){return idRol;}
+    public void setIdRol(Long idRol){this.idRol = idRol;}
+
+}
