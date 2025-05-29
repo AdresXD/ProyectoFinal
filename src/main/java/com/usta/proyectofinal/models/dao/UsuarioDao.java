3790c881b550379ea04d7a4ede63c84b9501a3e7
AndreXD <jarl020905@gmail.com>
2025-05-28 23:59:23 -0500

    Agregados mentoriaDao y UsuarioDao, Aún no están completos, asi que se deben seguir actualizando

---
+++ src/main/java/com/usta/proyectofinal/models/dao/UsuarioDao.java
@@ -0,0 +1,15 @@
+package com.usta.proyectofinal.models.dao;
+
+import com.usta.proyectofinal.entities.UsuarioEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UsuarioDao extends CrudRepository<UsuarioEntity, Long> {
+
+    @Transactional
+    @Query("SELECT Us FROM UsuarioEntity Us WHERE Us.email=?1")
+    public UsuarioEntity findByEmail(String email);
+
+
+}
